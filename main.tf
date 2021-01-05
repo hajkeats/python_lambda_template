330e9a5513f46c05c8f2290537bae16eaaaa369b
hajkeats <hajkeats@hotmail.com>
2021-01-05 10:11:30 +0000

    removal of interpolation only value

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "aws_cloudwatch_log_group" "lambda_log_group" {
 }
 
 resource "aws_iam_role_policy_attachment" "basic_lambda_policy" {
-  role       = "${aws_iam_role.lambda_role.name}"
+  role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
